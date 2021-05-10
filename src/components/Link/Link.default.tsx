1b5058cd92e8e648950ed814d8bc0a5c1a2e5deb
Georgina Serra <georgina@futureworkshops.com>
2021-05-10 14:20:57 +0200

    Adding the atribute linkStrokeDasharray

--- src/components/Link/Link.default.tsx
+++ src/components/Link/Link.default.tsx
@@ -47,10 +47,10 @@ export const LinkDefault = ({
       : generateRightAnglePath(startPos, endPos)
     : generateCurvePath(startPos, endPos);
 
-  const linkColor: string =
-    (fromPort.properties && fromPort.properties.linkColor) || "cornflowerblue";
-  const linkStrokeWidth: string =
-    (fromPort.properties && fromPort.properties.linkStrokeWidth) || "3";
+  const linkColor: string = fromPort?.properties?.linkColor || "cornflowerblue";
+  const linkStrokeWidth: string = fromPort?.properties?.linkStrokeWidth || "3";
+  const linkStrokeDasharray: string =
+    fromPort?.properties?.linkStrokeDasharray || "none";
 
   return (
     <svg
@@ -69,6 +69,7 @@ export const LinkDefault = ({
         stroke={linkColor}
         strokeWidth={linkStrokeWidth}
         fill="none"
+        strokeDasharray={linkStrokeDasharray}
       />
       {/* Thick line to make selection easier */}
       <path
--- stories/index.tsx
+++ stories/index.tsx
@@ -13,6 +13,7 @@ import { ExternalReactState } from "./ExternalReactState";
 import { InternalReactState } from "./InternalReactState";
 import { LinkColors } from "./LinkColors";
 import { LinkStrokeWidths } from "./LinkStrokeWidths";
+import { LinkStrokeDasharray } from "./LinkStrokeDasharray";
 import { ReadonlyMode } from "./ReadonlyMode";
 import { SelectedSidebar } from "./SelectedSidebar";
 import { SmartRouting } from "./SmartRouting";
@@ -30,6 +31,7 @@ storiesOf("Custom Components", module)
   .add("Canvas Outer", CustomCanvasOuterDemo)
   .add("Canvas Link", () => <CustomLinkDemo />)
   .add("Link Colors", () => <LinkColors />)
+  .add("Link Dotted", () => <LinkStrokeDasharray />)
   .add("Link Stroke Widths", () => <LinkStrokeWidths />);
 
 storiesOf("Stress Testing", module).add("default", StressTestDemo);
